e5e6a394268a4c783b40e706ddf5dda266fe5ae7
MattiasKDev <mattias@kerner.ca>
2025-03-29 14:43:41 -0400

    created searchbar and results page

---
+++ frontend/components/card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-white text-black rounded-lg shadow-md p-4">
+    <h2 class="text-lg font-bold">{{ name }}</h2>
+    <p class="text-gray-700">Price: ${{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ frontend/pages/results/[query].vue
@@ -0,0 +1,17 @@
+<script setup>
+const route = useRoute();
+console.log(typeof route.params.query);
+const query = route.params.query;
+const results = getResults(query);
+</script>
+<template>
+  <div class="grid-flow-row">
+    <div>
+      <card
+        v-for="result in results"
+        :name="result.name"
+        :price="result.price"
+      />
+    </div>
+  </div>
+</template>
