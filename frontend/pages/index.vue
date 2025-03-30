7d0dedb70c4dc90b274bac0c18e502830c7b3b75
Phineas Luu <truongluu2703@gmail.com>
2025-03-29 20:17:25 -0400

    Geolocation at created

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="min-h-screen flex justify-center items-center">
-    <search/>
-    <grid/>
+    <search @keyup.enter="showGrid = true" v-model="searchQuery"/>
+    <h1 ></h1>
+    <grid v-if="showGrid"/>
   </div>
 </template>
 
 <script>
 export default {
-  // data() {
-  //   return {
-  //     searchQuery: "",
-  //   };
-  // },
+  data() {
+    return {
+      showGrid: false,
+    };
+  },
   // methods: {
   //   goToResults() {
   //     if (this.searchQuery.trim()) {
@@ -19,5 +20,25 @@ export default {
   //     }
   //   },
   // },
+  
+  created() {
+    const getGeolocation = async () => {
+      if (navigator.geolocation) {
+      try {
+        const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+        console.log("Latitude:", position.coords.latitude);
+        console.log("Longitude:", position.coords.longitude);
+      } catch (error) {
+        console.error("Error getting geolocation:", error.message);
+      }
+      } else {
+      console.error("Geolocation is not supported by this browser.");
+      }
+    };
+
+    getGeolocation();
+  }
 };
 </script>
